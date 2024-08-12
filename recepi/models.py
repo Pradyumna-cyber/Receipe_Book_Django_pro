26747532045102e6b3c6451e0909138a3ca5af0f
Pradyumna Aher <73057121+Pradyumna-cyber@users.noreply.github.com>
2024-08-12 18:22:27 +0530

    Delete recepi directory

--- recepi/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Receipe(models.Model):
-    name = models.CharField(max_length=255)
-    descp = models.TextField()
-    img=models.ImageField(upload_to="receipe")
